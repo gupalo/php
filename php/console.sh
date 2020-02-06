fad6fa0ddcf707703804f42ee456116bf1bb94ae
vl <vl@playtini.ua>
2020-02-06 13:44:26 +0200

    allow /opt/php/console.sh with fixed count of runs

--- php/console.sh
+++ php/console.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+[[ -z ${RUNS} ]] && RUNS=0
+
+FOREVER_RUNS=0
+if [ ${RUNS} -eq 0 ]; then
+    FOREVER_RUNS=1
+    RUNS=1
+fi
 
 . ${DIR}/init.sh
 
-while true; do
+while [ ${RUNS} -gt 0 ]; do
     ${CONSOLE} "$@" ${DEBUG_PARAMS}
-    sleep ${SLEEP}
+
+    if [ ${FOREVER_RUNS} -eq 0 ]; then
+        RUNS=$((${RUNS} - 1))
+    fi
+    if [ ${RUNS} -gt 0 ]; then
+        sleep ${SLEEP}
+    fi
 done
