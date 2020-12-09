3fb48c44e5425d77186dc8e099fafe9fad5f3087
vl <vladimir.luchaninov@gmail.com>
2020-12-09 16:39:36 +0200

    added database autocreation and nicer doctrine migrations

--- php/main.sh
+++ php/main.sh
@@ -27,6 +27,8 @@ echo "Waiting for lock (main)..."
 
     if [ -d /code/src/Migrations ] || [ -d /code/migrations ] ; then
         (
+            ${CONSOLE} doctrine:database:create --if-not-exists
+            ${CONSOLE} doctrine:migrations:sync-metadata-storage
             ${CONSOLE} doctrine:migrations:migrate -n
         )
     fi
